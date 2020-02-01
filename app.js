fb6219a4bd2cb0f7ae7bdf0e951346d90ca1d686
syntakks <syntakks@gmail.com>
2020-02-01 00:32:16 -0600

    io.ON updated. Player class added
    Creating events for registering / spawning / and disconnecting players.

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 var io = require('socket.io')(process.env.PORT || 52300); 
 
+// Custom Classes
+var Player = require('./models/Player')
+
 console.log('Server has started!')
+
+// Dictionary of our players. 
+var players = []
+var sockets = []
 
 io.on('connection', function(socket) {
     console.log('Connection Made!')
 
+    var player = new Player()
+    var thisPlayerID = player.id
+
+    players[thisPlayerID] = player
+    sockets[thisPlayerID] = socket
+
+     // Tell the client that this is our id for the server. 
+    socket.emit('register', { id: thisPlayerID })
+    socket.emit('spawn', player) // Tell myself that I have spawned.
+    socket.broadcast.emit('spawn', player) // Tell others that I have spawned. 
+
+    // Tell myself about everyone else in the game. 
+    for (var playerID in players) {
+        if (playerID != thisPlayerID) { //  Ignore self. 
+            socket.emit('spawn', players[playerID])
+        }
+    }
+
     socket.on('disconnect', function() {
         console.log('A player has disconnected!')
+        delete players[thisPlayerID]
+        delete sockets[thisPlayerID]
+        socket.broadcast.emit('disconnected', player)
     })
-    
 })
